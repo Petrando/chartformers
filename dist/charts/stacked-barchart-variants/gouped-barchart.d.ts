c99defd736e6c47d4d423903b3638525373b2563
Petrando <petrandorichard@gmail.com>
2025-12-10 19:20:51 +0800

    commit after new build

--- dist/charts/stacked-barchart-variants/gouped-barchart.d.ts
+++
@@ -1,3 +0,0 @@
-import { default as React } from 'react';
-import { StackedBarChartProps } from './types';
-export declare function GroupedBarChart({ data, colorIdx, orientation }: StackedBarChartProps): React.JSX.Element;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
 export { BarChart } from './charts/barchart';
 export { PieChart } from './charts/piechart';
 export { StackedBarChart } from './charts/stacked-barchart-variants/stacked-barchart';
-export { GroupedBarChart } from './charts/stacked-barchart-variants/gouped-barchart';
+export { GroupedBarChart } from './charts/stacked-barchart-variants/grouped-barchart';
 export { PercentageBarChart } from './charts/stacked-barchart-variants/percentage-barchart';

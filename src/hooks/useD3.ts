cf662e848ee7c8b57fe1549e54369f4a59169f35
Petrando <petrandorichard@gmail.com>
2026-01-18 13:38:20 +0700

    fix: useD3 hook only imports neccessary module from d3

--- src/hooks/useD3.ts
+++ src/hooks/useD3.ts
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import * as d3 from "d3";
+import { select } from "d3";
 
 // T = type of the DOM element (default to HTMLDivElement)
 export const useD3 = <T extends HTMLElement | SVGSVGElement>(
@@ -10,7 +10,7 @@ export const useD3 = <T extends HTMLElement | SVGSVGElement>(
 
   useEffect(() => {
     if (dependencies !== undefined && ref.current) {
-      renderChartFn(d3.select(ref.current));
+      renderChartFn(select(ref.current));
     }
   }, dependencies === null ? undefined : dependencies); // eslint-disable-line react-hooks/exhaustive-deps
 

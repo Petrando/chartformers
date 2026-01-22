6dfb074cd9d017acc06ebe85a20a14d67d85fd86
Petrando <petrandorichard@gmail.com>
2026-01-22 12:15:56 +0700

    add population data

---
+++ dev/data/population-data.ts
@@ -0,0 +1,180 @@
+export type ageRangeGroup = {
+    ageRange: string;
+    male: number;
+    female: number;
+}
+export const population1:ageRangeGroup[] = [
+    {
+        "ageRange": "0-4",
+        "male": 8985,
+        "female": 8366
+    },
+    {
+        "ageRange": "5-9",
+        "male": 18039,
+        "female": 16363
+    },
+    {
+        "ageRange": "10-14",
+        "male": 21601,
+        "female": 19947
+    },
+    {
+        "ageRange": "15-19",
+        "male": 19275,
+        "female": 17721
+    },
+    {
+        "ageRange": "20-24",
+        "male": 15912,
+        "female": 14551
+    },
+    {
+        "ageRange": "25-29",
+        "male": 14061,
+        "female": 14325
+    },
+    {
+        "ageRange": "30-34",
+        "male": 17073,
+        "female": 18471
+    },
+    {
+        "ageRange": "35-39",
+        "male": 19745,
+        "female": 20542
+    },
+    {
+        "ageRange": "40-44",
+        "male": 23302,
+        "female": 22166
+    },
+    {
+        "ageRange": "45-49",
+        "male": 19566,
+        "female": 17278
+    },
+    {
+        "ageRange": "50-54",
+        "male": 14881,
+        "female": 12777
+    },
+    {
+        "ageRange": "55-59",
+        "male": 11397,
+        "female": 9879
+    },
+    {
+        "ageRange": "60-64",
+        "male": 8652,
+        "female": 7202
+    },
+    {
+        "ageRange": "65-69",
+        "male": 6061,
+        "female": 5646
+    },
+    {
+        "ageRange": "70-74",
+        "male": 4834,
+        "female": 4154
+    },
+    {
+        "ageRange": "75-79",
+        "male": 2211,
+        "female": 2331
+    },
+    {
+        "ageRange": "80+",
+        "male": 2358,
+        "female": 2647
+    }
+]
+
+export const population2: ageRangeGroup[] = [
+    {
+        ageRange: "0-4",
+        male: 9120,
+        female: 8210
+    },
+    {
+        ageRange: "5-9",
+        male: 17780,
+        female: 16640
+    },
+    {
+        ageRange: "10-14",
+        male: 21940,
+        female: 19680
+    },
+    {
+        ageRange: "15-19",
+        male: 18960,
+        female: 17940
+    },
+    {
+        ageRange: "20-24",
+        male: 16180,
+        female: 14290
+    },
+    {
+        ageRange: "25-29",
+        male: 13890,
+        female: 14510
+    },
+    {
+        ageRange: "30-34",
+        male: 16840,
+        female: 18710
+    },
+    {
+        ageRange: "35-39",
+        male: 20010,
+        female: 20390
+    },
+    {
+        ageRange: "40-44",
+        male: 22980,
+        female: 22440
+    },
+    {
+        ageRange: "45-49",
+        male: 19810,
+        female: 17020
+    },
+    {
+        ageRange: "50-54",
+        male: 14650,
+        female: 12990
+    },
+    {
+        ageRange: "55-59",
+        male: 11180,
+        female: 10040
+    },
+    {
+        ageRange: "60-64",
+        male: 8830,
+        female: 7050
+    },
+    {
+        ageRange: "65-69",
+        male: 6210,
+        female: 5480
+    },
+    {
+        ageRange: "70-74",
+        male: 4710,
+        female: 4320
+    },
+    {
+        ageRange: "75-79",
+        male: 2280,
+        female: 2250
+    },
+    {
+        ageRange: "80+",
+        male: 2420,
+        female: 2590
+    }
+];
